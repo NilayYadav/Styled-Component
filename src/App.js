ccf18ad29755b76c6b0fb5f20773aaea3aeeb2e4
NilayYadav <nilayyadva10@gmail.com>
2022-12-19 20:54:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  )
+}
+
+export default App
